e131d2261b6c0d83768e4232b397e6d01eb93cff
Joao Rodolpho <joaorodolphocabral@gmail.com>
2022-08-23 18:47:27 -0300

    avançando com a view

--- js/scrip.js
+++ js/scrip.js
@@ -2,7 +2,13 @@ function change_tab(id) {
   document.getElementById("page_content").innerHTML = document.getElementById(
     id + "_desc"
   ).innerHTML;
+
   document.getElementById("page1").className = "notselected btn btn-info";
   document.getElementById("page2").className = "notselected btn btn-info";
+}
+
+function page_open(id) {
+  document.getElementById("page_content").innerHTML =
+    document.getElementById("page1_desc").innerHTML;
   document.getElementById(id).className = "selected btn btn-info";
 }
